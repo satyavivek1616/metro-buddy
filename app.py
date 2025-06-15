8e3a881965c6a34513e684a437351769b13098fb
Vivek <vipparthivivek@gmail.com>
2025-06-15 11:00:03 +0530

    Updated app.py with new logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,5 +14,5 @@ def submit_location():
     print(f"Friend's location: {lat}, {lon}")  # Printed only in your terminal
     return "OK", 200
 
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
